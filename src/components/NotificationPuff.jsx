aeda0fbce90d7d0833be8fce0022a412b9cd58fd
Arihant Jain <jainarihant.aj20@gmail.com>
2024-08-16 20:42:13 +0530

    first commit

---
+++ src/components/NotificationPuff.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import "./style.css";
+
+export const NotificationPuff = () => {
+    return (
+        <div className="notification-PUFF">
+            <div className="overlap">
+                <img className="rectangle" alt="Rectangle" src="rectangle-8.png" />
+                <div className="base" />
+                <div className="navbar">
+                    <div className="group" />
+                </div>
+                <div className="ellipse" />
+                <img className="vector" alt="Vector" src="vector.svg" />
+                <div className="overlap-group-wrapper">
+                    <div className="overlap-group">
+                        <img className="clip-path-group" alt="Clip path group" src="clip-path-group.png" />
+                        <div className="SPACE-MISSIONS">
+                            SPACE <br />
+                            MISSIONS
+                        </div>
+                    </div>
+                </div>
+                <img className="bildschirmfoto" alt="Bildschirmfoto" src="bildschirmfoto-2024-08-09-um-17-08-09-1.png" />
+                <div className="overlap-wrapper">
+                    <div className="div">
+                        <img className="img" alt="Rectangle" src="rectangle-12.png" />
+                        <div className="ellipse-2" />
+                        <div className="div-wrapper">
+                            <div className="ellipse-wrapper">
+                                <div className="ellipse-3" />
+                            </div>
+                        </div>
+                        <button className="button">
+                            <div className="overlap-group-2">
+                                <img className="rectangle-2" alt="Rectangle" src="rectangle-12-2.svg" />
+                                <div className="text-wrapper">Staking Page</div>
+                            </div>
+                        </button>
+                        <img className="kopie-von-UI-SPACE" alt="Kopie von UI SPACE" src="character_front.png" />
+                        <img className="kopie-von-UI-SPACE-2" alt="Kopie von UI SPACE" src="character_back.png" />
+                        <div className="text-wrapper-2">750 $PUFF</div>
+                        <p className="p">Galaxy Pass #13 - Tier II</p>
+                        <img className="ellipse-4" alt="Ellipse" src="Ellipse 22.png" />
+                        <img
+                            className="design-ohne-titel"
+                            alt="Design ohne titel"
+                            src="middleEmoji.png"
+                        />
+                    </div>
+                </div>
+                <div className="group-2">
+                    <div className="overlap-group-2">
+                        <img className="rectangle-3" alt="Rectangle" src="Rectangle 12.png" />
+                        <div className="text-wrapper-3">50% Claimed</div>
+                    </div>
+                </div>
+                <button className="group-wrapper">
+                    <div className="group-3">
+                        <div className="overlap-group-2">
+                            <img className="rectangle-3" alt="Rectangle" src="Rectangle 11.png" />
+                            <div className="text-wrapper-4">50% Staked</div>
+                        </div>
+                    </div>
+                </button>
+                <img className="ellipse-5" alt="Ellipse" src="ellipse-24.svg" />
+                <img className="ellipse-6" alt="Ellipse" src="ellipse-177.svg" />
+            </div>
+            <div className="text-wrapper-5">IRL</div>
+        </div>
+    );
+};
